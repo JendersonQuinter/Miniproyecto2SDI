2dfc6d08187ef02295614a0d71e03682ab9a486d
JendersonQuinter <j.quintero@correo.unimet.edu.ve>
2021-03-07 11:01:24 -0400

    Creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,10 +12,28 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 
 // Importación del Ambiente
 import { environment } from 'src/environments/environment';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ListaPeliculasComponent } from './pages/lista-peliculas/lista-peliculas.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PeliculaComponent } from './components/pelicula/pelicula.component';
+import { ReservarComponent } from './components/reservar/reservar.component';
+import { FavoritosComponent } from './pages/favoritos/favoritos.component';
+import { FavoritoComponent } from './components/favorito/favorito.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ListaPeliculasComponent,
+    NavbarComponent,
+    LoginComponent,
+    FooterComponent,
+    PeliculaComponent,
+    ReservarComponent,
+    FavoritosComponent,
+    FavoritoComponent
   ],
   imports: [
     BrowserModule,
