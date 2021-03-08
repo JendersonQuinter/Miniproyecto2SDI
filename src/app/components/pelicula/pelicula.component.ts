93dfbea841953d71e4e914ef9a7f5363dc805120
JendersonQuinter <j.quintero@correo.unimet.edu.ve>
2021-03-08 03:56:32 -0400

    Creación de detalles peliculas

--- src/app/components/pelicula/pelicula.component.ts
+++ src/app/components/pelicula/pelicula.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import firebase from 'firebase';
+import { AuthService } from 'src/app/servicios/auth.service';
 
 @Component({
   selector: 'app-pelicula',
@@ -9,11 +11,16 @@ export class PeliculaComponent implements OnInit {
 
   img = '';
   activador = false;
+  user: firebase.User = null;
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
     this.img = 'https://i.pinimg.com/236x/cd/3d/b2/cd3db2881015941b4e5f124344405727--disney-films-comic-book.jpg';
+    // tslint:disable-next-line: deprecation
+    this.authService.getCurrentUser().subscribe((user) => {
+    this.user = user;
+    });
   }
 
   mostrarDetalles(): void {
